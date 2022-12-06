b98c7e737ed72374c247b0be4d14477a009506f2
Jota Zen <jotazents@gmail.com>
2022-12-06 18:55:20 -0300

    Added marker for services, labels

--- src/javascript/mapview.js
+++ src/javascript/mapview.js
@@ -23,11 +23,35 @@ map.on('click', function (m) {
   console.log(m.latlng)
   coordenadas.innerHTML = `>lat: ${m.latlng.lat}, lng: ${m.latlng.lng}`
 })
-
 // Recuperar datos del formulario
-fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vSsvgLtFTpz7lsQBIzEsQYAsT0FeuMfw_lzb3dcOrs0QgQFsCTeTTz8cSXEVpY36EUnn_StGf_qpgeL/pub?gid=1770946352&single=true&output=csv')
-  .then((response) => response.text())
-  .then((data) => {
-    console.log(data)
-  });
-  
+async function getData(){
+  let response = await fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vSsvgLtFTpz7lsQBIzEsQYAsT0FeuMfw_lzb3dcOrs0QgQFsCTeTTz8cSXEVpY36EUnn_StGf_qpgeL/pub?gid=1770946352&single=true&output=csv')
+  let data = await response.text()
+  let filas = data.split('\n')
+  filas = filas.map((fila) => fila.split(',')).slice(1)
+  filas.forEach((row) => {
+
+    const ltd = row[4].split(" ").filter((x) => {
+      x = parseInt(x)
+      if (x == NaN) {}
+      else {return x}
+    })
+    const lng = row[5].split(" ").filter((x) => {
+      x = parseInt(x)
+      if (x == NaN) {}
+      else {return x}
+    })
+    lng[0] = lng[0].split('"')[0]
+
+    // Añadir marcadores de encuestas al mapa
+    const circulo = L.circle([ltd[0],lng[0]], {
+      color: sample.compañias[row[1]].color,
+      fillColor: sample.compañias[row[1]].color,
+      fillOpacity: 0.2,
+      radius: row[2] !== 'Satelital' ? 550 : 1000
+    }).addTo(map)
+    circulo.bindPopup(`${row[1]}\n${row[2]}\n${row[3]}\n`)
+  })
+}
+getData()
+
---
+++ src/javascript/sample_data.js
@@ -0,0 +1,27 @@
+const sample = {
+  compañias: {
+      Clero:{
+        antenas: [
+          {
+            nombre: "antena_1",
+            coordenadas: [-37.469167, -72.509444],
+            alcance: {
+              max: 10,
+              min: 1
+            },
+            servicios: ["2G","3G"]
+          }
+        ],
+        color: "#D73030"
+      },
+      Mow:{ 
+        color: "#8E21A2"
+      },
+      Emtel:{
+        color: "#6077DC"
+      },
+      Mobistar:{
+        color: "#30D7D3"
+      }
+  }
+}
